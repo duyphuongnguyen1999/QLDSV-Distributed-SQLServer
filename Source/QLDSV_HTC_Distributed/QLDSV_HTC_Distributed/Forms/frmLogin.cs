dd8a48c4304dda2703d597e807642bf7b0c8010b
duyphuongnguyen1999 <duyphuongnguyen1999@gmail.com>
2025-11-08 12:26:43 +0700

    feat: MVP version. Only check "Roll" and "Khoa"

--- Source/QLDSV_HTC_Distributed/QLDSV_HTC_Distributed/Forms/frmLogin.cs
+++ Source/QLDSV_HTC_Distributed/QLDSV_HTC_Distributed/Forms/frmLogin.cs
@@ -14,75 +14,61 @@ namespace QLDSV_HTC
 
         private void InitializeComponent()
         {
-            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.cboRole = new System.Windows.Forms.ComboBox();
             this.txtPassword = new System.Windows.Forms.TextBox();
             this.cboKhoa = new System.Windows.Forms.ComboBox();
-            this.cboRole = new System.Windows.Forms.ComboBox();
             this.btnLogin = new System.Windows.Forms.Button();
             this.btnExit = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
-
+          
             // label1
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(30, 30);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(100, 17);
-            this.label1.Text = "Tên đăng nhập:";
-
-            // txtUsername
-            this.txtUsername.Location = new System.Drawing.Point(150, 27);
-            this.txtUsername.Name = "txtUsername";
-            this.txtUsername.Size = new System.Drawing.Size(200, 22);
-
-            // label2
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(30, 70);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(70, 17);
-            this.label2.Text = "Mật khẩu:";
-
-            // txtPassword
-            this.txtPassword.Location = new System.Drawing.Point(150, 67);
-            this.txtPassword.Name = "txtPassword";
-            this.txtPassword.Size = new System.Drawing.Size(200, 22);
-            this.txtPassword.UseSystemPasswordChar = true;
-
-            // label3
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(30, 110);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(60, 17);
-            this.label3.Text = "Vai trò:";
-
+            this.label1.Text = "Vai trò:";
+            
             // cboRole
             this.cboRole.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cboRole.FormattingEnabled = true;
             this.cboRole.Items.AddRange(new object[] { "PGV", "KHOA", "SV", "PKT" });
-            this.cboRole.Location = new System.Drawing.Point(150, 107);
+            this.cboRole.Location = new System.Drawing.Point(150, 27);
             this.cboRole.Name = "cboRole";
             this.cboRole.Size = new System.Drawing.Size(200, 24);
             this.cboRole.SelectedIndexChanged += new System.EventHandler(this.cboRole_SelectedIndexChanged);
 
-            // label4
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(30, 150);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(45, 17);
-            this.label4.Text = "Khoa:";
+            // label2
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 70);
+            this.label2.Name = "label3";
+            this.label2.Size = new System.Drawing.Size(45, 17);
+            this.label2.Text = "Khoa:";
 
             // cboKhoa
             this.cboKhoa.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cboKhoa.FormattingEnabled = true;
-            this.cboKhoa.Location = new System.Drawing.Point(150, 147);
+            this.cboKhoa.Location = new System.Drawing.Point(150, 67);
             this.cboKhoa.Name = "cboKhoa";
             this.cboKhoa.Size = new System.Drawing.Size(200, 24);
 
+            // label3
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 110);
+            this.label3.Name = "label2";
+            this.label3.Size = new System.Drawing.Size(70, 17);
+            this.label3.Text = "Mật khẩu:";
+
+            // txtPassword
+            this.txtPassword.Location = new System.Drawing.Point(150, 107);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(200, 22);
+            this.txtPassword.UseSystemPasswordChar = true;
+
             // btnLogin
-            this.btnLogin.Location = new System.Drawing.Point(80, 200);
+            this.btnLogin.Location = new System.Drawing.Point(80, 170);
             this.btnLogin.Name = "btnLogin";
             this.btnLogin.Size = new System.Drawing.Size(100, 35);
             this.btnLogin.Text = "Đăng nhập";
@@ -90,7 +76,7 @@ namespace QLDSV_HTC
             this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
 
             // btnExit
-            this.btnExit.Location = new System.Drawing.Point(220, 200);
+            this.btnExit.Location = new System.Drawing.Point(220, 170);
             this.btnExit.Name = "btnExit";
             this.btnExit.Size = new System.Drawing.Size(100, 35);
             this.btnExit.Text = "Thoát";
@@ -98,16 +84,14 @@ namespace QLDSV_HTC
             this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
 
             // frmLogin
-            this.ClientSize = new System.Drawing.Size(400, 270);
+            this.ClientSize = new System.Drawing.Size(400, 220);
             this.Controls.Add(this.btnExit);
             this.Controls.Add(this.btnLogin);
-            this.Controls.Add(this.cboKhoa);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.cboRole);
-            this.Controls.Add(this.label3);
             this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cboKhoa);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.cboRole);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
@@ -118,33 +102,31 @@ namespace QLDSV_HTC
             this.PerformLayout();
         }
 
-        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.ComboBox cboRole;
         private System.Windows.Forms.TextBox txtPassword;
         private System.Windows.Forms.ComboBox cboKhoa;
-        private System.Windows.Forms.ComboBox cboRole;
         private System.Windows.Forms.Button btnLogin;
         private System.Windows.Forms.Button btnExit;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
 
         private void LoadComboKhoa()
         {
             cboKhoa.Items.Add("CNTT - Công nghệ thông tin");
             cboKhoa.Items.Add("VT - Viễn thông");
-            cboKhoa.SelectedIndex = 0;
+            cboKhoa.SelectedIndex = 0; 
         }
 
         private void cboRole_SelectedIndexChanged(object sender, EventArgs e)
         {
-            // PGV có thể chọn khoa, các role khác không
+            // Only "KHOA" role could select, either couldn't
             if (cboRole.SelectedItem != null)
             {
                 string role = cboRole.SelectedItem.ToString();
-                cboKhoa.Enabled = (role == "PGV" || role == "KHOA");
+                cboKhoa.Enabled = (role == "KHOA");
 
-                if (role == "PKT" || role == "SV")
+                if (role == "PGV" || role == "PKT" || role == "SV")
                 {
                     cboKhoa.Enabled = false;
                 }
@@ -153,17 +135,6 @@ namespace QLDSV_HTC
 
         private void btnLogin_Click(object sender, EventArgs e)
         {
-            // Đơn giản hóa: không kiểm tra username/password thực tế
-            // Chỉ kiểm tra các trường đã điền đầy đủ
-
-            if (string.IsNullOrEmpty(txtUsername.Text))
-            {
-                MessageBox.Show("Vui lòng nhập tên đăng nhập!", "Thông báo",
-                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                txtUsername.Focus();
-                return;
-            }
-
             if (cboRole.SelectedItem == null)
             {
                 MessageBox.Show("Vui lòng chọn vai trò!", "Thông báo",
@@ -174,7 +145,7 @@ namespace QLDSV_HTC
 
             string role = cboRole.SelectedItem.ToString();
 
-            if ((role == "PGV" || role == "KHOA") && cboKhoa.SelectedItem == null)
+            if ((role == "KHOA") && cboKhoa.SelectedItem == null)
             {
                 MessageBox.Show("Vui lòng chọn khoa!", "Thông báo",
                     MessageBoxButtons.OK, MessageBoxIcon.Warning);
@@ -182,25 +153,18 @@ namespace QLDSV_HTC
                 return;
             }
 
-            // Lưu thông tin đăng nhập
+            // Save login information
             DatabaseConnection.UserRole = role;
 
-            if ((role == "PGV" || role == "KHOA") && cboKhoa.SelectedItem != null)
+            if ((role == "KHOA") && cboKhoa.SelectedItem != null)
             {
+                // Get khoaText and padding 10 character (similar to database)
                 string khoaText = cboKhoa.SelectedItem.ToString();
-                // Lấy mã khoa và pad với space để = 10 ký tự (giống database)
                 string maKhoa = khoaText.StartsWith("CNTT") ? "CNTT" : "VT";
-                DatabaseConnection.UserMaKhoa = maKhoa.PadRight(10);
-                DatabaseConnection.CurrentKhoa = DatabaseConnection.UserMaKhoa;
-            }
-            else
-            {
-                // Mặc định là CNTT cho SV và PKT
-                DatabaseConnection.UserMaKhoa = "CNTT      ";
-                DatabaseConnection.CurrentKhoa = "CNTT      ";
+                DatabaseConnection.CurrentKhoa = maKhoa.PadRight(10);
             }
 
-            // Mở form chính
+            // Open Main form
             this.Hide();
             frmMain mainForm = new frmMain();
             mainForm.ShowDialog();
